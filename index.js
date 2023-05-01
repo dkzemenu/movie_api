b9c589750cf81f93601e3aa646a4ab6656bcf74a
JosephHo7 <118572699+JosephHo7@users.noreply.github.com>
2023-05-01 18:07:53 -0400

    update mongoose connect

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const mongoose = require('mongoose');
 const Models = require('./models.js'); 
 const { check, validationResult} = require('express-validator');
+// const dotenv = require('dotenv');
+// dotenv.config();
 
 // mongoose.connect('mongodb://127.0.0.1:27017/cfDB', { useNewUrlParser: true, useUnifiedTopology: true});
 
 // mongoose.connect('mongodb+srv://JosephHo7:Yellowminion2@myflixdb.dqwxcwl.mongodb.net/myFlixDB?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true});
-mongoose.connect('process.env.CONNECTION_URI', { useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect( process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true});
 
 
 const Movies = Models.Movie;
