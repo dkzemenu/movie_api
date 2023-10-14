39a255bd7d8e47ca99ea188b4920eae605ed11a3
JosephHo7 <118572699+JosephHo7@users.noreply.github.com>
2023-10-13 20:09:08 -0400

    update index

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const { check, validationResult} = require('express-validator');
 // const dotenv = require('dotenv');
 // dotenv.config();
 
+//for local deploy
 // mongoose.connect('mongodb://127.0.0.1:27017/cfDB', { useNewUrlParser: true, useUnifiedTopology: true});
 
 mongoose.connect( process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true});
